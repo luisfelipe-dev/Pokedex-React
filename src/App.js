0ad1ed2caa604531d2aa07b3791580bcebd8367e
Luis Felipe <degaus@live.com>
2020-03-31 00:55:15 -0300

    Adicionado o Axios ao projeto e adiiconado a lista no useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+import api from './services/api';
 
 import './global.css';
 
 function App() {
-  return <h1>Pokédex With React</h1>;
+  const [pokemons, setPokemons] = useState([]);
+
+  const loadPokemons = async () => {
+    const allPokemons = [];
+    try {
+      for (let i = 1; i < 150; i++) {
+        await allPokemons.push(api.get(`${i}`).then(res => res.data));
+      }
+
+      Promise.all(allPokemons).then(pokemons => setPokemons(pokemons));
+    } catch (err) {
+      console.log('caiu no erro');
+    }
+  };
+
+  useEffect(() => {
+    loadPokemons();
+  }, []);
+
+  return (
+    <>
+      <ul>
+        {pokemons.map(pokemon => (
+          <li key={pokemon.id}>
+            {pokemon.name}
+            <img src={`https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png`} alt={pokemon.name}/>
+          </li>
+        ))}
+      </ul>
+      
+    </>
+  );
 }
 
 export default App;
